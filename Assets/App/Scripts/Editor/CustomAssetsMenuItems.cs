518da8e4c27a0c7fe6e0d11ec75b0f0045558607
Shintaro Tanikawa <shintaro.tanikawa@gmail.com>
2020-02-06 12:42:08 +0900

    Add a menu item 'Assets/Create/GameScene'.

---
+++ Assets/App/Scripts/Editor/CustomAssetsMenuItems.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Chsopoly.Editor
+{
+    static class CustomAssetsMenuItems
+    {
+        const int CreatePriority = 10000;
+
+        [MenuItem ("Assets/Create/GameScene", false, CreatePriority)]
+        static void GenerateVariantPrefab ()
+        {
+            EditorUtility.FocusProjectWindow ();
+
+            var path = AssetDatabase.GenerateUniqueAssetPath (GetCurrentFolder () + "/NewScene.prefab");
+            var source = Resources.Load ("BasePrefabs/GameScene");
+            var prefab = (GameObject) PrefabUtility.InstantiatePrefab (source);
+            var newObj = PrefabUtility.SaveAsPrefabAsset (prefab, path);
+            GameObject.DestroyImmediate (prefab);
+
+            Selection.activeObject = newObj;
+        }
+
+        static string GetCurrentFolder ()
+        {
+            string filePath;
+            if (Selection.assetGUIDs.Length == 0)
+            {
+                // No asset selected.
+                filePath = "Assets";
+            }
+            else
+            {
+                // Get the path of the selected folder or asset.
+                filePath = AssetDatabase.GUIDToAssetPath (Selection.assetGUIDs[0]);
+
+                // Get the file extension of the selected asset as it might need to be removed.
+                string fileExtension = Path.GetExtension (filePath);
+                if (fileExtension != "")
+                {
+                    filePath = Path.GetDirectoryName (filePath);
+                }
+            }
+
+            return filePath;
+        }
+    }
+}
