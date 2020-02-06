38eaa6538caafe2ca495a4274a1bb39aa78f866d
Shintaro Tanikawa <shintaro.tanikawa@gmail.com>
2020-02-06 12:43:03 +0900

    Auto addressable assets importer was added.

---
+++ Assets/App/Scripts/BaseSystem/AddressableAssets/Editor/AddressableAssetsNameBuilder.cs
@@ -0,0 +1,63 @@
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.AddressableAssets;
+
+namespace Chsopoly.BaseSystem.AddressableAssets.Editor
+{
+    public class AddressableAssetsNameBuilder : AssetPostprocessor
+    {
+        const string AssetRootPath = "Assets/App/AddressableAssets";
+
+        [MenuItem ("Project/AddressableAssets/Refresh All AddressableAssets Entries")]
+        static void BuildAllAddressableAssetsNames ()
+        {
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            var group = settings.DefaultGroup;
+            foreach (var entry in group.entries.ToArray ())
+            {
+                settings.RemoveAssetEntry (entry.guid);
+            }
+            foreach (var path in Directory.GetFiles (AssetRootPath, "*.*", SearchOption.AllDirectories))
+            {
+                if (path.EndsWith (".meta"))
+                {
+                    continue;
+                }
+
+                SetAddressableAssetsName (path);
+            }
+        }
+
+        static void OnPostprocessAllAssets (string[] importedAssets,
+            string[] deletedAssets,
+            string[] movedAssets,
+            string[] movedFromAssetPaths)
+        {
+            foreach (var asset in importedAssets.Union (movedAssets))
+            {
+                if (!asset.StartsWith (AssetRootPath))
+                {
+                    continue;
+                }
+                SetAddressableAssetsName (asset);
+            }
+        }
+
+        static void SetAddressableAssetsName (string path)
+        {
+            var settings = AddressableAssetSettingsDefaultObject.Settings;
+            var group = settings.DefaultGroup;
+            var guid = AssetDatabase.AssetPathToGUID (path);
+            var entry = group.GetAssetEntry (guid);
+            if (entry != null)
+            {
+                entry.SetAddress (path);
+            }
+            else
+            {
+                settings.CreateOrMoveEntry (guid, group, true);
+            }
+        }
+    }
+}
